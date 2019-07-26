65687cb94b1f8cd31fbde93259e9c874f290e0ef
thiphan <thoaptk07@gmail.com>
2019-07-25 19:28:52 -0500

    product creating page, url added

--- products/urls.py
+++ products/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from products import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home, name='home'),
-    path('accounts/',include('accounts.urls')),
-    path('products/',include('products.urls')),
+    path('create',views.create, name='create'),
+
 ]
