8de1cfa604d7e7e9238c05c5259dc3ab8715d811
thiphan <thoaptk07@gmail.com>
2019-07-25 21:58:10 -0500

    home page

--- products/views.py
+++ products/views.py
@@ -6,7 +6,8 @@ from django.utils import timezone
 
 # Create your views here.
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html',{'products':products})
 
 @login_required
 def create(request):
